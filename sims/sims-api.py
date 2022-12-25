862ec073f89115b4c73edfdda201dae3419d14e1
Dwi Taniel <dc@dwichandra.info>
2022-12-25 18:30:41 +0400

    Pushing from notebook to github

--- sims/sims-api.py
+++ sims/sims-api.py
@@ -5,3 +5,14 @@ app = Flask(__name__)
 @app.route('/')
 def root():
     return 'you have landed safely'
+
+
+import random
+
+# List of data set elements
+data_set = [1, 2, 3, 4, 5]
+
+# Select a random element from the data set
+random_element = random.choice(data_set)
+print(random_element)
+
